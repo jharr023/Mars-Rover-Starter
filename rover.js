55042cd5d927401eddc921eea8de6372fe318a64
jharr023 <jharringtonkc@gmail.com>
2024-02-26 18:21:39 -0600

    commit 2

--- rover.js
+++ rover.js
@@ -1,5 +1,13 @@
 class Rover {
-   // Write code here!
-}
-
-module.exports = Rover;
+   constructor(position) {
+     this.position = position;
+     this.mode = 'NORMAL';
+     this.generatorWatts = 110;
+   }
+ 
+   receiveMessage(message) {
+     // Implementation for handling messages
+   }
+ }
+ 
+ module.exports = Rover;
--- spec/message.spec.js
+++ spec/message.spec.js
@@ -6,4 +6,18 @@ const Command = require('../command.js');
 
 describe("Message class", function() {
 
+    it("throws error if a name is NOT passed into the constructor as the first parameter", function() {
+      expect( function() { new Message(); }).toThrow(new Error('Name is required for a message.'));
+    });
+    
+    it("constructor sets name", function() {
+        const message = new Message('Test message', []);
+        expect(message.name).toBe('Test message');
+    });
+
+    it("contains a commands array passed into the constructor as the 2nd argument", function() {
+        const commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+        const message = new Message('Test message', commands);
+        expect(message.commands).toEqual(commands);
+      });
 });
